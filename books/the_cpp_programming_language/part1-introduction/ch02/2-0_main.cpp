5a57bf56910bf12df0190c18d421bb3d86472fed
Greg Tate <gregt8@gmail.com>
2025-04-15 03:07:31 -0500

    Chapter 2: Add minimal main function example from "The C++ Programming Language"

---
+++ books/the_cpp_programming_language/part1-introduction/ch02/2-0_main.cpp
@@ -0,0 +1,9 @@
+/*
+ * Program: Minimal Main Function
+ * Context: Chapter 2.0 from "The C++ Programming Language" by Bjarne Stroustrup
+ * Author: Greg Tate
+ * Date: April 14, 2025
+ * Synopsis: This program demonstrates the simplest possible C++ program with an empty main function.
+ */
+
+int main() {}
