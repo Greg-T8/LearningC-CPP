fe62763f1f3e2c748a2e92f45c81c64a8b56f184
Greg Tate <gregt8@gmail.com>
2025-04-15 03:07:39 -0500

    Chapter 2: Add Hello, World! program from "The C++ Programming Language"

---
+++ books/the_cpp_programming_language/part1-introduction/ch02/2-1_hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * Program: Hello, World!
+ * Context: Chapter 2.1 from "The C++ Programming Language" by Bjarne Stroustrup
+ * Author: Greg Tate
+ * Date: April 14, 2025
+ * Synopsis: This program prints "Hello, World!" to the console.
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+}
