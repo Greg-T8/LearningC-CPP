a0f51c957f1c45cb09119583737642adc36eb1bc
Greg Tate <gregt8@gmail.com>
2025-04-15 03:43:12 -0500

    Chapter 2: Add program to demonstrate sizeof operator for fundamental C++ types

---
+++ books/the_cpp_programming_language/part1-introduction/ch02/2-03_sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Size chart of C++ fundamental types:\n";
+    std::cout << "bool: " << sizeof(bool) << " byte\n";
+    std::cout << "char: " << sizeof(char) << " byte\n";
+    std::cout << "int: " << sizeof(int) << " bytes\n";
+    std::cout << "float: " << sizeof(float) << " bytes\n";
+    std::cout << "double: " << sizeof(double) << " bytes\n";
+}
