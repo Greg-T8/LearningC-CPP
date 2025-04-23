3fcb8312da0c40de8f2d91470781ba2ee411ede8
Greg Tate <gregt8@gmail.com>
2025-04-23 04:49:46 -0500

    Ch02: Update variable initialization examples for clarity and correctness

--- books/the_cpp_programming_language/part1-introduction/ch02/2-05_initialization.cpp
+++ books/the_cpp_programming_language/part1-introduction/ch02/2-05_initialization.cpp
@@ -15,5 +15,7 @@ int main() {
     std::cout << "Demonstrating initialization of variables:\n";
     int i1 = 7.2;   // Implicit conversion from double to int
     int i2{7};      // Corrected: Removed narrowing conversion
-    int i3 = {7};   // Corrected: Fixed syntax error
+    int i3 {7};     // Allows for spaces in initialization
+    int i4{7.2};    // error: floating-point integer narrowing conversion
+    int i5 = {7};   // Corrected: Fixed syntax error
 }
