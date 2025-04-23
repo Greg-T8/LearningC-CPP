b2d624af3befd5af785fef8571e7d0c00acadd3c
Greg Tate <gregt8@gmail.com>
2025-04-23 05:18:04 -0500

    Ch02: Add constants example with constant and constexpr variables

---
+++ books/the_cpp_programming_language/part1-introduction/ch02/2-06_constants.cpp
@@ -0,0 +1,5 @@
+int main() {
+    const int dmv = 17;     // Declare a constant integer variable dmv and initialize it to 17
+    int var = 17;           // Declare a regular integer variable var and initialize it to 17
+    constexpr double max1 = 1.4 * square(dmv);      // Declare a constant expression variable max1 and initialize it to 1.4 times the square of dmv
+}
