740d1d5c35b2fdbe126c5189cf28855b9e0ad001
Greg Tate <gregt8@gmail.com>
2025-04-30 05:29:24 -0500

    Ch01: Add initial implementation for character counting program

---
+++ books/the_c_programming_language/ch01/character_counting/ex1_8_character_count.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int character, blanks, tabs, newlines;
+    blanks = tabs = newlines = 0;
+
+    while((character = getchar()) != EOF) {
+        if (character == ')
+    }
+}
