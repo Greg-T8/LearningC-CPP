b2d380dffb6a6eceeabe9ab85302035b34b25c41
Greg Tate <gregt8@gmail.com>
2025-05-10 03:56:48 -0500

    Ch01: Notes - Enable additional compiler warnings in build configuration

--- books/the_c_programming_language/ch01/word_counting/word_count.c
+++ books/the_c_programming_language/ch01/word_counting/word_count.c
@@ -12,18 +12,15 @@ int main()
     state = OUT;
     nl = nw = nc = 0;
 
-    while ((c = getchar()) != EOF)
-    {
+    while ((c = getchar()) != EOF) {
         ++nc;
-        if (c == '\n')
-        {
+        if (c == '\n') {
             ++nl;
         }
-        if (c == ' ' || c == '\n' || c == '\t')
-        {
+        if (c == ' ' || c == '\n' || c == '\t') {
             state = OUT;
-        } else if (state == OUT)
-        {
+        }
+        else if (state == OUT) {
             state = IN;
             ++nw;
         }
