900c1736f30dc51a1b167b823d51fbb1956500b3
Greg Tate <gregt8@gmail.com>
2025-05-22 04:31:51 -0500

    Ch01: Notes - Refactor echo_input.c for improved word separation logic and update test.txt with additional input parameters

--- books/the_c_programming_language/ch01/word_counting/ex1-12/echo_input.c
+++ books/the_c_programming_language/ch01/word_counting/ex1-12/echo_input.c
@@ -1,16 +1,32 @@
+/*
+    Program: Echo Input One Word Per Line
+    Context: The C Programming Language, Chapter 1, Exercise 1-12
+    Description: Reads input and prints each word on a separate line.
+    Author: Greg Tate
+    Date: 2025-05-22
+*/
+
 #include <stdio.h>
 
 int main()
 {
     int c;
 
+    // Read characters until EOF and print each word on a new line.
     while ((c = getchar()) != EOF) {
-        if (c != ' ' || c != '\n' || c != '\t') {
+        if (c == ' ' || c == '\t') {
+            putchar('\n');
+        }
+        else if (c == '\n') {
+            // No action needed for newlines
+        }
+        else if (c == '.' || c == ';' || c == ':') {
+            putchar('\n');
             putchar(c);
+            putchar('\n');
         }
         else {
-            putchar('\r');
-            putchar('\n');
+            putchar(c);
         }
     }
 }
