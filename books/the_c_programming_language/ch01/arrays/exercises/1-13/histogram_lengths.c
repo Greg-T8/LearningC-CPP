acacb3ed7782e96f1a16c5ca7b87339a15271b5c
Greg Tate <gregt8@gmail.com>
2025-08-03 06:03:31 -0500

    Ch01: Notes - Refactor word length histogram program for clarity and structure

--- books/the_c_programming_language/ch01/arrays/exercises/1-13/histogram_lengths.c
+++ books/the_c_programming_language/ch01/arrays/exercises/1-13/histogram_lengths.c
@@ -1,3 +1,10 @@
+/*
+    Program: Word Length Histogram
+    Author: Greg Tate
+    Date: 2025-08-03
+    Context: The C Programming Language, Chapter 1, Arrays, Exercise 1-13
+*/
+
 #include <stdio.h>
 
 int main()
@@ -8,20 +15,31 @@ int main()
     max_word_length = 20;
     int word_length_array[max_word_length + 1];
 
+    // Initialize the word length array to zero
     for (int i = 0; i < (max_word_length + 1); i++)
         word_length_array[i] = 0;
 
+    // Read input and count word lengths
     word_length = 0;
-    while ((c = getchar()) != EOF)
-        if (c == ' ' || c == '\n' || c == '\t') {
-            if (word_length > 0 && word_length < max_word_length)
-                word_length_array[word_length]++;
-            word_length = 0;
-        }
-        else
-            word_length++;
+    while ((c = getchar()) != EOF) {
 
-    printf("Word Length Histogram");
+        // Check if character is part of a word
+        if (c != ' ' && c != '\n' && c != '\t') {
+            word_length++;
+        }
+        else {
+            // If word boundary is reached, update histogram and reset word length
+            if (word_length > 0 && word_length < max_word_length) {
+                word_length_array[word_length]++;
+            }
+            else {
+                // Reset word length if not a valid word
+                word_length = 0;
+            }
+    }
+
+    // Print the word length histogram
+    printf("Word Length Histogram:\n");
     for (int i = 0; i < (max_word_length + 1); i++)
         printf("%d: %d", i, word_length_array[i]);
 
